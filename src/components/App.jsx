875140942741471197f49e11f98d1248619dee3a
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-11 01:10:04 +0900

    2.3: 준비, 다크모드로 바꿔보자

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,31 +5,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius:50px;
-`;
-
-const Text = styled.span`
- color: white;
-`;
-
 export default function App() {
   return (
-    <Father>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </Father>
+    <Father></Father>
   );
 }
