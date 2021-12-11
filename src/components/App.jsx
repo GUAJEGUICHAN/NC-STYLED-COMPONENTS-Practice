61b9d77b97e84c856f16c8251abe11402c259163
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-11 14:55:21 +0900

    2.5: ⭐️styled component를 직접 지목해서 css 속성 넣기

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,6 +19,10 @@ const animation = keyframes`
   }
 `
 
+const Emoji = styled.span`
+    font-size:48px
+`;
+
 const Box = styled.div`
   height:200px;
   width:200px;
@@ -27,10 +31,9 @@ const Box = styled.div`
   justify-content:center;
   align-items:center;
   animation:${animation} 1s linear infinite;
-  span{
-    font-size:80px;
+  ${Emoji}{
     &:hover{
-      font-size:40px;
+      font-size:98px;
     }
     &:active{
       opacity:0;
@@ -42,7 +45,7 @@ export default function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😁</span>
+        <Emoji>😁</Emoji>
       </Box>
       <Box />
       <Box />
