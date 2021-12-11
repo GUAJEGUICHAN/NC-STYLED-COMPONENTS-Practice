ed32b4fd2b8ab5c1ae4ca55f46bf358d1dea5a8d
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-11 19:06:46 +0900

    2.7: ⭐️ThemeProvider 사용해서 적용해보기

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,23 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
+const Title = styled.h1`
+  color:${(props) => props.theme.textColor}
+`;
+
 const Wrapper = styled.div`
   display:flex;
-`;
-
-const animation = keyframes`
-  0%{
-    transform:rotate(0deg);
-    border-radius:0px;
-  }50%{
-    transform:rotate(180deg);
-    border-radius:100px
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`
-
-const Emoji = styled.span`
-    font-size:48px
-`;
-
-const Box = styled.div`
-  height:200px;
-  width:200px;
-  background-color: tomato;
-  display:flex;
+  height: 100vh;
+  width: 100vw;
   justify-content:center;
   align-items:center;
-  animation:${animation} 1s linear infinite;
-  ${Emoji}{
-    &:hover{
-      font-size:98px;
-    }
-    &:active{
-      opacity:0;
-    }
-  }
+  background-color:${props => props.theme}
 `;
 
 export default function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😁</Emoji>
-      </Box>
-      <Box />
-      <Box />
-      <Box />
-      <Box />
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
