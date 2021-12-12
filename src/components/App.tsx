00ac8c102001e24b7a72f1f4eabd35c7a97032cd
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-13 00:13:53 +0900

    3.5: typescript form실습 환경 세팅

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,8 +7,7 @@ import Circle from './Circle';
 export default function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle text="i'm here" bgColor="tomato" />
+
     </div>
   );
 }
