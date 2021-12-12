2cc24f39b5ffc733d6a0ded1bd52e2c633dcc140
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-13 00:03:45 +0900

    3.4:⭐️typescript는 useState에서 초기값에 따라 자동으로 타입을 넣어준다

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 interface ContainerProps {
@@ -21,6 +21,8 @@ interface CircleProps {
 }
 
 export default function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  const [counter, setCounter] = useState(1)//초기 값에 따라서 typescript가 자동으로 해당 변수의 타입을 정한다. 여기서는 number로 정해졌다.
+  //setCounter("2") //typescript가 자동으로 에러를 감지한다. 왜냐하면 string 타입이 들어갔기 떄문이다.
   return <Container bgColor={bgColor} borderColor={borderColor ?? "red"} >
     {text}
   </Container>;
