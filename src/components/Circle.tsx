3584e81a087cf72ca260f61a173dbb32d885f814
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-12 23:49:03 +0900

    3.3: ⭐️⭐️borderColor을 optional props로 설정! 어느 props에서 borderColor가 optional인지 required인지 유심히 보자 borderColor가 undefined일때 대체값을 넣어뭐야한다.

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;//여기에는 required로 되어있다.
 }
 
 const Container = styled.div<ContainerProps>`
@@ -10,12 +11,14 @@ const Container = styled.div<ContainerProps>`
   width: 100px;
   height: 100px;
   border-radius:50px;
+  border: 4px solid ${props => props.borderColor}
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;//optional로 설정!
 }
 
-export default function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+export default function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? "red"} />;//Container입장에서는 borderColor가 required라서 값이 반드시 필요하다 ??로 undefined 값일 때 대체할 수 있는 값을 입력한다.
 }
