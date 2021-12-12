866b9dd5fae80f4f12aec9ad2b8fa5355f1f52ab
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-13 00:20:27 +0900

    3.5: ⭐️⭐️ form태그에서 event에 타입을 넣는방법

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Circle from './Circle';
 
 
 
 export default function App() {
+  const [value, setValue] = useState("")
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    //위의 형태는 굉장히 직관적이지 않다. 제대로 사용하려면 구글에 검색하는 것 말고는 없다.
+    //typescript는 onChange함수가 InputElement에 의해서 실행될 것을 안다.
+    console.log(event.currentTarget.value);
+  }
   return (
     <div>
-
+      <form action="">
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder='username'
+        />
+      </form>
     </div>
   );
 }
