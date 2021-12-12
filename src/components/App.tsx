61d10019b936a4a14ee18b1cf4bbf8cfeba98f23
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-12 23:32:44 +0900

    3.2: ⭐️interface 사용해서 원하는 문자열 출력

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+
 import Circle from './Circle';
+interface PlayerShape {
+  name: string;
+  age: string;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+  `Hello ${playerObj.name} you are ${playerObj.age} years old.`
 
 export default function App() {
+  console.log(sayHello({ name: "nico", age: "12" }))
   return (
     <div>
       <Circle bgColor="teal" />
