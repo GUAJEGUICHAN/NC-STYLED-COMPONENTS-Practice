85651096937e1d88b31211f4ffc0b339f17ef2b8
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-13 00:22:15 +0900

    3.5: ⭐️ form 태그에서 input 태그 작동시키기

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,9 +7,10 @@ import Circle from './Circle';
 export default function App() {
   const [value, setValue] = useState("")
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    //위의 형태는 굉장히 직관적이지 않다. 제대로 사용하려면 구글에 검색하는 것 말고는 없다.
-    //typescript는 onChange함수가 InputElement에 의해서 실행될 것을 안다.
-    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value)
   }
   return (
     <div>
