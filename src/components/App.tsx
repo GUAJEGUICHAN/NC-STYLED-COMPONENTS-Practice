84dd78970a35bac17ce25de3a9beaba3df8ed782
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-13 15:17:33 +0900

    4.1: css 스타일 초기화 / 폰트 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,53 @@
 import React, { useState } from 'react';
 
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Router from '../Router';
 import Circle from './Circle';
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor}
-`;
-const H1 = styled.h1`
-  color:${[(props) => props.theme.textColor]}
+const GlobalStyle = createGlobalStyle`
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+</style>
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    font-size:14px;
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  }
+  a{
+    text-decoration:none;
+    color:inherit;
+    cursor: pointer;
+  }
+  ol, ul, li {
+      list-style: none;
+  }
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    }
+  input, button {
+    background-color: transparent;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family:'Maven Pro', sans-serif;
+  }
+  body{
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+  a{
+    text-decoration:none;
+  }
 `;
 
 export default function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Router />
-    </div>
+    </>
 
   );
 }
