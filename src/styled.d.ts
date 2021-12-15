071c2694d40fdfb3ff60071e76ee59e0c3488c9e
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-16 01:14:28 +0900

    4.1: css 스타일 초기화 / 폰트 추가

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -6,5 +6,6 @@ declare module 'styled-components' {
   export interface DefaultTheme {//이것이 우리가 사용할 테마이다. 
     textColor: string;
     bgColor: string;
+    accentColor: string;
   }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black"
-}
-
-export const darkTheme: DefaultTheme = {
   bgColor: "black",
-  textColor: "white"
+  textColor: "white",
+  accentColor: "#44bd32"
 }
