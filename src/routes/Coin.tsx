18aba92223bc1416bbe23bfc7bdffd657663c5d8
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-16 23:48:24 +0900

    4.8 chart와 price로 가는 링크 생성

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-import { Outlet, useLocation, useParams } from "react-router-dom";
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 const Title = styled.h1`
   colors:${(props) => props.theme.accentColor}
 `;
@@ -176,11 +176,12 @@ export default function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItems>
           </Overview>
-          <Outlet />
-
+          <Link to={`/${coinId}/chart`}>Chart</Link>
+          <Link to={`/${coinId}/price`}>Price</Link>
         </>
       }
+
       <Outlet />
-    </Container>
+    </Container >
   )
 }
