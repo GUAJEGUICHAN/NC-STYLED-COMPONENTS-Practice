7ca0084e93754f8c96e5efde8a24067e9cc56430
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-07 18:32:17 +0900

    5.2 리코일 설치 / 사용 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil'
 
 import App from './components/App';
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
+  <>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
+  </>
+  ,
   document.getElementById('app'),
 );
